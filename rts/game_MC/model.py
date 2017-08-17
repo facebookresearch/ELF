ffb6bf062e8de424126dd4218159448d5b3d257c
Yuandong Tian <yuandong@fb.com>
2017-08-17 16:15:09 -0700

    Add arch switch

--- rts/game_MC/model.py
+++ rts/game_MC/model.py
@@ -19,21 +19,27 @@ class MiniRTSNet(Model):
         self.m = args.params["num_unit_type"] + 7
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.num_channels = [ self.m, 64, 64, 128, 128, 128, 64, 64, self.m ]
+        # self.arch = "ccpccp"
+        # self.arch = "ccccpccccp"
+        self.arch, channels = self.args.arch.split(";")
+
+        self.num_channels = []
+        for v in channels.split(","):
+            if v == "-":
+                self.num_channels.append(self.m)
+            else:
+                self.num_channels.append(int(v))
 
         self.convs = [ nn.Conv2d(self.num_channels[i], self.num_channels[i+1], 3, padding = 1) for i in range(len(self.num_channels)-1) ]
         for i, conv in enumerate(self.convs):
-            setattr(self, "conv%d" % i, conv)
+            setattr(self, "conv%d" % (i + 1), conv)
 
         self.relu = nn.ReLU() if self._no_leaky_relu() else nn.LeakyReLU(0.1)
 
         if not self._no_bn():
             self.convs_bn = [ nn.BatchNorm2d(conv.out_channels) for conv in self.convs ]
             for i, conv_bn in enumerate(self.convs_bn):
-                setattr(self, "conv%d_bn" % i, conv_bn)
-
-        # self.arch = "ccpccp"
-        self.arch = "ccccpccccp"
+                setattr(self, "conv%d_bn" % (i + 1), conv_bn)
 
     def _no_bn(self):
         return getattr(self.args, "disable_bn", False)
@@ -77,6 +83,11 @@ class Model_ActorCritic(Model):
         self.linear_policy = nn.Linear(linear_in_dim, params["num_action"])
         self.linear_value = nn.Linear(linear_in_dim, 1)
         self.softmax = nn.Softmax()
+
+    def get_define_args():
+        return [
+            ("arch", "ccccpccccp;-,64,64,128,128,128,64,64,-")
+        ]
 
     def forward(self, x):
         if self.params.get("model_no_spatial", False):
