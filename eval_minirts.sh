4e996218df840fa0dd010ad9ba6b943b20f38bda
Yuandong Tian <yuandong@fb.com>
2017-08-18 14:09:11 -0700

    Update eval_minirts.sh

--- eval_minirts.sh
+++ eval_minirts.sh
@@ -1,4 +1,4 @@
 MODEL=$1
 FRAMESKIP=$2
 
-eval_only=1 game=./rts/game_MC/game model_file=./rts/game_MC/model model=actor_critic taskset -c 12-23 python3 eval.py  --num_games 128 --batchsize 32 --tqdm --load ${MODEL} --eval_gpu 4 --fs_opponent ${FRAMESKIP} --latest_start 0 --opponent_type AI_SIMPLE --eval_stats winrate --num_eval 10000 --additional_labels id,last_terminal
+eval_only=1 game=./rts/game_MC/game model_file=./rts/game_MC/model model=actor_critic taskset -c 12-23 python3 eval.py  --num_games 128 --batchsize 32 --tqdm --load ${MODEL} --gpu 4 --fs_opponent ${FRAMESKIP} --latest_start 0 --opponent_type AI_SIMPLE --eval_stats winrate --num_eval 10000 --additional_labels id,last_terminal
