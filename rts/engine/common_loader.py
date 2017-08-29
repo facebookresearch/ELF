de45ce0c988ab5b3c8b940a5d71df4bc65361e31
Yuandong Tian <yuandong@fb.com>
2017-08-29 16:23:26 -0700

    Add error info when assignment in CommonLoader goes wrong

--- rts/engine/common_loader.py
+++ rts/engine/common_loader.py
@@ -1,3 +1,5 @@
+import sys
+
 from elf import GCWrapper, ContextArgs
 from rlpytorch import ArgsProvider
 
@@ -34,12 +36,16 @@ class CommonLoader:
         # Can we automate this?
         bool_convert = dict(t=True, true=True, f=False, false=False)
 
-        if key == "fow":
-            setattr(ai_options, key, bool_convert[value.lower()])
-        elif key == "name" or key == "args" or key == "type":
-            setattr(ai_options, key, value)
-        else:
-            setattr(ai_options, key, int(value))
+        try:
+            if key == "fow":
+                setattr(ai_options, key, bool_convert[value.lower()])
+            elif key == "name" or key == "args" or key == "type":
+                setattr(ai_options, key, value)
+            else:
+                setattr(ai_options, key, int(value))
+        except ValueError:
+            print("Value error! key = " + str(key) + " value = " + str(value))
+            sys.exit(1)
 
     def _parse_players(self, opt, player_names):
         players_str = str(self.args.players)
