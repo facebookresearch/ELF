9b7774a5dd713f80654437922bfb66200a86ebb9
Yuandong Tian <yuandong@fb.com>
2017-09-06 11:09:54 -0700

    Add bn to the model

--- go/df_model.py
+++ go/df_model.py
@@ -17,24 +17,29 @@ class Model_Policy(Model):
         # print("#num_planes: " + str(self.num_planes))
 
         # Simple method. multiple conv layers.
+        self.dim = 128
         self.convs = []
+        self.convs_bn = []
         last_planes = self.num_planes
         for i in range(10):
-            conv = nn.Conv2d(last_planes, 128, 3, padding=1)
+            conv = nn.Conv2d(last_planes, self.dim, 3, padding=1)
+            conv_bn = nn.BatchNorm2d(self.dim)
             setattr(self, "conv" + str(i), conv)
+            setattr(self, "conv_bn" + str(i), conv_bn)
             self.convs.append(conv)
-            last_planes = 128
+            self.convs_bn.append(conv_bn)
+            last_planes = self.dim
 
-        self.final_conv = nn.Conv2d(128, self.num_future_actions, 3, padding=1)
+        self.final_conv = nn.Conv2d(self.dim, self.num_future_actions, 3, padding=1)
 
         # Softmax as the final layer
         self.softmax = nn.Softmax()
-        self.relu = nn.ReLU()
+        self.relu = nn.LeakyReLU(0.1)
 
     def forward(self, x):
         s = self._var(x["features"])
-        for conv in self.convs:
-            s = self.relu(conv(s))
+        for conv, conv_bn in zip(self.convs, self.convs_bn):
+            s = conv_bn(conv(s))
 
         output = self.final_conv(s)
         actions = []
