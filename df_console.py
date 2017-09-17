81ca88acfb692b4c66966edbeea66bb3903c82bf
Yuandong Tian <yuandong@fb.com>
2017-09-17 14:21:54 -0700

    Add model.eval() and fix the issues between training and eval with batchsize = 1

--- df_console.py
+++ df_console.py
@@ -162,9 +162,10 @@ class DFConsole:
         GC = env["game"].initialize()
         model = env["model_loaders"][0].load_model(GC.params)
         mi = ModelInterface()
-        model.eval()
         mi.add_model("model", model, optim_params={ "lr" : 0.001})
         mi.add_model("actor", model, copy=True, cuda=True, gpu_id=args.gpu)
+        mi["model"].eval()
+        mi["actor"].eval()
 
         self.evaluator = evaluator
         self.last_move_idx = None
