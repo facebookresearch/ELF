de36e6551a4d5732e148815193a7b71681b33342
yuandong <yuandong.tian@gmail.com>
2017-11-08 14:23:03 -0800

    Update lua script

--- rts/engine/test2.lua
+++ rts/engine/test2.lua
@@ -3,7 +3,10 @@
 
 -- Attribute that are not specified but defined in C struct 
 -- will use its default value defined in C, or remain undefined.
---
+function test()
+    print("Hello from LUA")
+end
+
 g_cmd_init_states = {
     ["gather"] = { 
         gather_state = 0
@@ -15,24 +18,21 @@ g_cmd_init_states = {
 --
 g_cmds = { }
 function g_init_cmd_start(cmd_id, name)
-    init_state = g_cmd_init_states[name]
     cmd = { _func = g_funcs[name] }
+    init_state = g_cmd_init_states[name]
+
     if init_state ~= nil then
-        table.insert(cmd, table.clone(init_state))
+        for k, v in pairs(init_state) do
+            cmd[k] = v
+        end
     end
     g_cmds[cmd_id] = cmd
 end
 
-function g_init_cmd(cmd_id, k, v)
-    g_cmds[cmd_id][k] = v
-end
-
 function g_run_cmd(cmd_id, env)
     cmd = g_cmds[cmd_id]
-    if cmd == nil then 
-        return false
-    end
-    cmd._func(env, cmd)
+    if cmd == nil then return global.CMD_FAILED end 
+    return cmd._func(env, cmd) or global.CMD_NORMAL
 end
 
 unit_init_attributes = {
@@ -59,17 +59,16 @@ unit_init_attributes = {
 
 g_funcs = { }
 function g_funcs.attack(env, cmd)
-     print("In attack!")
      local target = env:unit(cmd.target)
      local u = env:self()
 
      if target:isdead() or not u:can_see(target) then
          -- c_print("Task finished!")
-         return global.COMPLETE
+         return global.CMD_COMPLETE
      end
      local att_r = u:att_r()
-     if u:cd_expired(global.CD_ATTACK) and env:dist_sqr(target:p()) then
-         -- c_print("Attacking .. ")
+     if u:cd_expired(global.CD_ATTACK) and env:dist_sqr(target:p()) < att_r * att_r then
+         -- print("Attacking .. ")
          -- Then we need to attack.
          if att_r <= 1.0 then
              env:send_cmd_melee_attack(cmd.target, u:att())
@@ -77,22 +76,24 @@ function g_funcs.attack(env, cmd)
              env:send_cmd_emit_bullet(cmd.target, u:att())
          end
      else
-         -- c_print("Moving towards target .. ")
-         env:move_towards(cmd.target)
+         -- print("Moving towards target .. ")
+         env:move_towards(target)
     end
+    -- print("Done with Attacking .. ")
 end
 
 function g_funcs.gather(env, cmd)
     local resource = env:unit(cmd.resource)
-    local base = env:find_closest_base()
+    local base = env:unit(cmd.base)
     local u = env:self()
 
     if resource:isdead() or base:isdead() then
-        return global.COMPLETE
+        -- print("no resource or base, done")
+        return global.CMD_COMPLETE
     end
     if cmd.gather_state == 0 then
         if env:move_towards(resource) < 1 then
-            env:start_cd(global.CD_GATHER)
+            env:cd_start(global.CD_GATHER)
             cmd.gather_state = 1
         end
     elseif cmd.gather_state == 1 then
@@ -115,25 +116,23 @@ function g_funcs.build(env, cmd)
     local u = env:self()
 
     if cmd.build_state == 0 then
-        if env:dist_sqr(cmd.build_p) < kBuildDistSqr then
+        if not cmd.p:isvalid() or env:dist_sqr(cmd.p) < kBuildDistSqr then
             env:send_cmd_change_resource(u:player_id(), -cost) 
-            env:start_cd(global.CD_BUILD)
+            env:cd_start(global.CD_BUILD)
             cmd.build_state = kBuilding
         else
-            if cmd.p:isvalid() then
-                local nearby_p = env:get_nearby_location(cmd.p)
-                env:move_towards_location(nearby_p)
-            end
+            local nearby_p = env:find_nearby_empty_place(cmd.p)
+            env:move_towards_location(nearby_p)
         end
     elseif cmd.build_state == kBuilding then
         if u:cd_expired(global.CD_BUILD) then
             local build_p = cmd.p
             if not build_p:isvalid() then
-                build_p = env:get_nearby_location(u:p()) 
+                build_p = env:find_nearby_empty_place(u:p()) 
             end
             if build_p:isvalid() then
                 env:send_cmd_create(cmd.build_type, build_p, u:player_id(), cost) 
-                return global.COMPLETE
+                return global.CMD_COMPLETE
             end
         end
     end
