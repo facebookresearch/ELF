b2e640c3df32dbc28ed286d42695149a6eb07f9c
Yuandong Tian <yuandong@fb.com>
2017-12-03 10:36:43 -0800

    Fix issues in train_atari.sh

--- train_atari.sh
+++ train_atari.sh
@@ -1,4 +1,4 @@
 ROM=$1
 GPU=$2
 
-game=./atari/game model=actor_critic model_file=./atari/model python3 train.py --rom_file $ROM --batchsize 4 --freq_update 1 --num_games 16 --tqdm --gpu $GPU --trainer_stats rewards --additional_labels id,last_terminal
+game=./atari/game model=actor_critic model_file=./atari/model python3 train.py --rom_file $ROM --batchsize 4 --freq_update 1 --num_games 16 --tqdm --gpu $GPU --trainer_stats rewards --additional_labels id,last_terminal --keys_in_reply V,rv
