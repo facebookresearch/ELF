9222e1597da74fcb27f86ef006e32dbab0b9c6ea
Yuandong Tian <yuandong@fb.com>
2017-12-07 09:52:08 -0800

    Update atari eval

--- eval_atari.sh
+++ eval_atari.sh
@@ -2,4 +2,4 @@ ROM=$1
 MODEL=$2
 GPU=$3
 
-game=./atari/game model=actor_critic model_file=./atari/model python3 eval.py --num_games 128 --batchsize 32 --tqdm --gpu $GPU --rom_file $ROM --load $MODEL --eval_stats rewards --reward_clip -1 --num_eval 500 --additional_labels id,last_terminal 
+game=./atari/game model=actor_critic model_file=./atari/model python3 eval.py --num_games 128 --batchsize 32 --tqdm --gpu $GPU --rom_file $ROM --load $MODEL --eval_stats rewards --reward_clip -1 --num_eval 500 --additional_labels id,last_terminal --keys_in_reply V,rv 
