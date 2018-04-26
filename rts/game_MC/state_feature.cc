c4936994797a9dd91018aa025bcd051adcf18cd9
Shubho Sengupta <ssengupta@fb.com>
2018-04-25 21:36:58 -0600

    Change License to BSD

--- rts/game_MC/state_feature.cc
+++ rts/game_MC/state_feature.cc
@@ -1,3 +1,11 @@
+/**
+* Copyright (c) 2017-present, Facebook, Inc.
+* All rights reserved.
+
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
 #include "state_feature.h"
 
 static inline void accu_value(int idx, float val, std::map<int, std::pair<int, float> > &idx2record) {
